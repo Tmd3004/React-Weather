e144d4a05b3d587d8e4ec522291d29effb6cd662
Manh Duc <tranmanhduc072@gmail.com>
2023-02-02 21:29:58 +0700

    Add new project food

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '21b50d47f7mshb394ad8eb0ccb5bp1099e1jsnb1a356ac4cf9',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+
+export const WEATHER_API_KEY = '2b7fd93e15ba48fb5b0795c7fb4bdc42';
+
